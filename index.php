c932c20d9277d39cf8e7f5542703ec1a9e90b1a4
swn <swnhieian@126.com>
2015-03-25 17:01:27 +0800

    first test commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>WeiConnet</title>
+</head>
+<body>
+Hello, world!
+<a href="http://www.baidu.com">百度</a>
+
+</body>
+</html>
