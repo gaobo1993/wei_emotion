69986fa422561f8fb45a3595e9567cb5882efbe2
swn <swnhieian@126.com>
2015-03-25 19:46:16 +0800

    test

--- home.php
+++ home.php
@@ -8,6 +8,7 @@
 <body>
 It works!
 <br/>
+
 <?php
 $code = $_GET['code'];
 $url = "https://api.weibo.com/oauth2/access_token?client_id=3128512954&client_secret=f4b76f3f0ebf32b31e06748cb10b6327&grant_type=authorization_code&redirect_uri=weiconnect.coding.io/home.php&code=" . $code;
@@ -15,7 +16,11 @@ $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_POST, 1);
 $json = curl_exec($curl);
 curl_close($curl);
+echo "<hr/>";
+echo $json;
+echo "<hr/>";
 $obj = json_decode($json);
+var_dump($obj);
 $token = $obj->access_token;
 $uid = $obj->uid;
 //get user information
