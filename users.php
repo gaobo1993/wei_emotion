fd5e3a4455b4dbff4821849855d99e544ed04f97
swnhieian <swnhieian@126.com>
2015-03-26 17:27:53 +0800

    test

--- users.php
+++ users.php
@@ -1,5 +1,5 @@
 <?php
-echo "here all users";
+@header('Content-type: text/html;charset=UTF-8');
 $mysqli = new mysqli("10.9.1.188", "LW70AGqB1OOFgzAO", "HJmN4DfBEnQ0ajEH", "cf_e61290b4_5735_47e5_891e_d13c3a00d3e3");
 if (mysqli_connect_error()) {
     die('Connect Error('.mysqli_connect_errno() .')'.mysqli_connect_error());
@@ -10,12 +10,14 @@ if ($stmt = $mysqli->prepare($query)) {
     $stmt->execute();
     $stmt->bind_result($id, $screen_name);
     while ($stmt->fetch()) {
-        printf("%d %s\n", $id, $screen_name);
+        $return[$id] = $screen_name;
     }
     $stmt->close();
 } else {
-echo "fail to lookup table".$mysqli->errno.":".$mysqli->error;
+    $return['result']='error';
+    $return[$mysqli->errno] = $mysqli->error;
 }
-
 $mysqli->close();
+$json = json_encode($return);
+echo $json;
 ?>
