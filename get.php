c21e271d8f26bcd5d13abf36d2218fa954e23a92
swnhieian <swnhieian@126.com>
2015-03-26 21:46:27 +0800

    test

--- get.php
+++ get.php
@@ -13,7 +13,9 @@ if ($result = $mysqli->query($query)) {
     $row = $result->fetch_array(MYSQLI_NUM);
     var_dump($row);
     $num = $row[0];
-} 
+    $result->free();
+} else {echo "select errro".$mysqli->errno.".".$mysqli->error;
+/*
 $query = "select "
 for ($i=0; $i<$num-1; $i ++) {
     $query .= ("post".$i.",");
@@ -25,7 +27,7 @@ if ($result = $mysqli->query($query)) {
     for($i=0; $i<$num; $i ++)
         echo $row[$i]."<hr/>";
 }
-
+*/
 
 $mysqli->close();
 
