03070cea5652ad16da7afb0469665a8e5518c70c
swnhieian <swnhieian@126.com>
2015-03-26 17:34:59 +0800

    test

--- users.php
+++ users.php
@@ -1,5 +1,4 @@
 <?php
-@header('Content-type: text/html;charset=UTF-8');
 $mysqli = new mysqli("10.9.1.188", "LW70AGqB1OOFgzAO", "HJmN4DfBEnQ0ajEH", "cf_e61290b4_5735_47e5_891e_d13c3a00d3e3");
 if (mysqli_connect_error()) {
     die('Connect Error('.mysqli_connect_errno() .')'.mysqli_connect_error());
