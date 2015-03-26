2fc9b3e8efaab7dded316545b47e56ca41dfa98c
swnhieian <swnhieian@126.com>
2015-03-26 21:34:45 +0800

    test

--- get.php
+++ get.php
@@ -7,24 +7,25 @@ if (mysqli_connect_error()) {
     die('Connect Error('.mysqli_connect_errno() .')'.mysqli_connect_error());
 }
 //show posts
-$query = "select num from users where uid = ".$uid;
+$query = "select num from users where id = ".$uid;
 $num = 0;
 if ($result = $mysqli->query($query)) {
     $row = $result->fecth_array(MYSQLI_NUM);
+    var_dump($row);
     $num = $row[0];
-}/*
+} 
 $query = "select "
 for ($i=0; $i<$num-1; $i ++) {
     $query .= ("post".$i.",");
 }
 $query .= ("post"+($num-1));
-$query .= " where uid=".$uid;
+$query .= " where id=".$uid;
 if ($result = $mysqli->query($query)) {
     $row = $result->fetch_array(MYSQLI_NUM);
     for($i=0; $i<$num; $i ++)
         echo $row[$i]."<hr/>";
 }
-*/
+
 
 $mysqli->close();
 
