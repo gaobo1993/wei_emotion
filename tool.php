2583924782152cd705feb284f50e25b0588c3d4c
swnhieian <swnhieian@126.com>
2015-03-29 19:48:41 +0800

    test

--- tool.php
+++ tool.php
@@ -43,7 +43,6 @@ function getkeywords($str) {
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     $keywords = curl_exec($curl);
-    var_dump($keywords);
     curl_close($curl);
     return $keywords;
 }
