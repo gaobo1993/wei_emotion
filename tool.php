e9cfa1ac14adb71a2da8520c9796b016be43443b
swnhieian <swnhieian@126.com>
2015-03-29 19:25:41 +0800

    test

--- tool.php
+++ tool.php
@@ -38,7 +38,7 @@ function JSON($array) {
 
 //get keywords
 function getkeywords($str) {
-    $url = "http://api.yutao.us/api/keyword/".$str;
+    $url = "http://api.yutao.us/api/keyword/".urlencode($str);
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
