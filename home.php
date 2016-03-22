433be99ddcf761946dbbea2254326a4a06a43bc5
swnhieian <swnhieian@126.com>
2016-03-22 18:52:58 +0800

    update home.php

--- home.php
+++ home.php
@@ -54,7 +54,7 @@ $json = curl_exec($curl);
 curl_close($curl);
 $user_array = json_decode($json, true);
 // connect mysql database
-$mysqli = new mysqli("10.9.1.188", "LW70AGqB1OOFgzAO", "HJmN4DfBEnQ0ajEH", "cf_e61290b4_5735_47e5_891e_d13c3a00d3e3");
+$mysqli = new mysqli("10.9.1.188", "bSjpPjgAFd4XHEhn", "KCuhIdJkhAqILkXS", "cf_2c9074ab_6d3e_4f67_93ef_fc0076f6c8b5");
 if (mysqli_connect_error()) {
     die('Connect Error('.mysqli_connect_errno() .')'.mysqli_connect_error());
 }
