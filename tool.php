10649e9ebdaacea10aaa2a18394be6ea9058991c
s0190 <gb12@mails.tsinghua.edu.cn>
2016-03-27 14:13:17 +0800

    update database

--- tool.php
+++ tool.php
@@ -38,12 +38,13 @@ function JSON($array) {
 
 //get keywords
 function getkeywords($str) {
-    $url = "http://api.yutao.us/api/keyword/".$str;
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    $keywords = curl_exec($curl);
-    curl_close($curl);
+    $keywords = myKeyword;
+    // $url = "http://api.yutao.us/api/keyword/".$str;
+    // $curl = curl_init();
+    // curl_setopt($curl, CURLOPT_URL, $url);
+    // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    // $keywords = curl_exec($curl);
+    // curl_close($curl);
     return $keywords;
 }
 ?>
