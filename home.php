06507cc54fa693e0f074fc85c4209e326bfa7dea
s0190 <gb12@mails.tsinghua.edu.cn>
2016-03-28 17:06:40 +0800

    emotion

--- home.php
+++ home.php
@@ -126,7 +126,7 @@ $json = curl_exec($curl);
 curl_close($curl);
 $user_array = json_decode($json, true);
 
-echo $user_array
+echo $user_array;
 
 $query = "insert into users(uid, screen_name, keywords) values (?,?,?)";
 if ($stmt = $mysqli->prepare($query)) {
