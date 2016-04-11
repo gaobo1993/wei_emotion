84970251439803786cc8f60f0267e5e57b8a3e42
s0190 <gb12@mails.tsinghua.edu.cn>
2016-04-11 20:34:43 +0800

    1

--- get.php
+++ get.php
@@ -3,7 +3,7 @@ require 'tool.php';
 @header("Content-Type:text/html;charset=utf-8");
 $uid = $_GET['uid'];
 $key = $_GET['keywords'];
-$mysqli mysqli("10.9.1.188", "qL8LQMhtBDyQgd0b", "JSfUTPlC0R3DthnG", "cf_afa98478_964a_43f8_a1c5_8193bff83c4e");
+$mysqli  = new mysqli("10.9.1.188", "qL8LQMhtBDyQgd0b", "JSfUTPlC0R3DthnG", "cf_afa98478_964a_43f8_a1c5_8193bff83c4e");
 if (mysqli_connect_error()) {
     die('Connect Error('.mysqli_connect_errno() .')'.mysqli_connect_error());
 }
